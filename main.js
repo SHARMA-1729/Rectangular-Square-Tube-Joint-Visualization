8e2333ca11f4b9af036b8b0d2431a9656ea55fc0
Saurav Sharma <sharma9310175627@gmail.com>
2025-11-19 19:23:01 +0530

    feat: add Electron main process

---
+++ main.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: false
+    }
+  })
+
+  win.loadFile('src/index.html')
+
+  win.webContents.openDevTools({ mode: 'detach' })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
